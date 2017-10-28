af566311f4a7d84f964700440bc4e19ff3ad0fde
Daniela Duricekova <daniela.duricekova@gmail.com>
2017-10-28 12:17:46 +0200

    Add main application.

---
+++ teeny_weeny_link/app.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#
+# Author:   Daniela Duricekova <daniela.duricekova@gmail.com>
+#
+
+"""Main application."""
+
+import flask
+import urllib
+
+from teeny_weeny_link.db import Database
+from teeny_weeny_link.id_generator import IDGenerator
+from teeny_weeny_link.config import parse_our_config_files
+
+
+app = flask.Flask(__name__)
+
+config = parse_our_config_files()
+db = Database(
+    gen=IDGenerator(config['id_generator']['salt']),
+    host=config['db']['host'],
+    port=config['db']['port'],
+    password=config['db']['password']
+)
+
+
+@app.route('/links', methods=['POST'])
+def insert_link():
+    link = flask.request.form.get('link')
+    if link is None:
+        body = {'error': "Missing link (pass 'link=URL' in POST data)!"}
+        return flask.jsonify(body), 400
+
+    link = _validate_link(link)
+
+    id = db.insert_link(link)
+
+    body = {
+        'id': id,
+        'visit_link': flask.url_for(
+            'visit_link',
+            id=id,
+            _external=True
+        ),
+        'visit_count_link': flask.url_for(
+            'get_visit_count',
+            id=id,
+            _external=True
+        )
+    }
+    return flask.jsonify(body)
+
+
+@app.route('/<string:id>')
+def visit_link(id):
+    link = db.visit_link(id)
+    if link is None:
+        body = {'error': 'No link for {}!'.format(id)}
+        return flask.jsonify(body), 404
+
+    body = {'location': link}
+    resp = flask.make_response(flask.jsonify(body))
+    resp.headers['Location'] = link
+    return resp, 301
+
+
+@app.route('/<string:id>/visit_count')
+def get_visit_count(id):
+    visit_count = db.get_visit_count(id)
+    if visit_count is None:
+        body = {'error': 'No link for {}!'.format(id)}
+        return flask.jsonify(body), 404
+
+    body = {'visit_count': visit_count}
+    return flask.jsonify(body)
+
+
+def _validate_link(link):
+    # The location header requires the link to have a scheme. Otherwise, if the
+    # link was e.g. www.google.com, it would be redirected to
+    # http://localhost/www.google.com.
+    parsed_link = urllib.parse.urlparse(link)
+    if not parsed_link.scheme:
+        link = 'http://' + link
+    return link
---
+++ tests/integration/test_app.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+#
+# Author:   Daniela Duricekova <daniela.duricekova@gmail.com>
+#
+
+"""Tests for the app module."""
+
+import json
+import unittest
+
+from teeny_weeny_link.app import app
+from teeny_weeny_link.app import db
+
+
+def json_from(response):
+    return json.loads(response.data.decode())
+
+
+class APPTests(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        db.remove_all_links()
+        self.client = app.test_client()
+
+    def test_insert_link_returns_correct_response(self):
+        original_link = 'http://www.google.com'
+
+        response = self.client.post(
+            '/links',
+            data={'link': original_link}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        body = json_from(response)
+        # id
+        self.assertIn('id', body)
+        id = body['id']
+        # visit_link
+        self.assertIn('visit_link', body)
+        visit_link = body['visit_link']
+        self.assertEqual(
+            visit_link,
+            'http://localhost/{}'.format(id)
+        )
+        # visit_count_link
+        self.assertIn('visit_count_link', body)
+        visit_count_link = body['visit_count_link']
+        self.assertEqual(
+            visit_count_link,
+            'http://localhost/{}/visit_count'.format(id)
+        )
+
+    def test_insert_link_adds_http_when_scheme_is_missing(self):
+        original_link = 'www.google.com'
+
+        response = self.client.post(
+            '/links',
+            data={'link': original_link}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        body = json_from(response)
+        visit_link = body['visit_link']
+        response = self.client.get(visit_link)
+        self.assertEqual(response.status_code, 301)
+        self.assertEqual(
+            response.headers['Location'],
+            'http://' + original_link
+        )
+
+    def test_insert_link_returns_error_when_link_is_missing(self):
+        response = self.client.post('/links')
+
+        self.assertEqual(response.status_code, 400)
+        body = json_from(response)
+        self.assertIn('error', body)
+        error = body['error']
+        self.assertIn('Missing link', error)
+
+    def test_visit_link_redirects_to_original_link(self):
+        original_link = 'http://www.google.com'
+        response = self.client.post(
+            '/links',
+            data={'link': original_link}
+        )
+        body = json_from(response)
+        visit_link = body['visit_link']
+
+        response = self.client.get(visit_link)
+        self.assertEqual(response.status_code, 301)
+        self.assertEqual(response.headers['Location'], original_link)
+        body = json_from(response)
+        # location
+        self.assertIn('location', body)
+        location = body['location']
+        self.assertEqual(location, original_link)
+
+    def test_visit_link_returns_error_when_id_does_not_exist(self):
+        id = 'err'
+
+        response = self.client.get('http://localhost/{}'.format(id))
+
+        self.assertEqual(response.status_code, 404)
+        body = json_from(response)
+        self.assertIn('error', body)
+        error = body['error']
+        self.assertIn(id, error)
+
+    def test_get_visit_count_returns_correct_visit_count(self):
+        original_link = 'http://www.google.com'
+        response = self.client.post(
+            '/links',
+            data={'link': original_link}
+        )
+        body = json_from(response)
+        visit_link = body['visit_link']
+        n = 3
+        for i in range(n):
+            self.client.get(visit_link)
+        visit_count_link = body['visit_count_link']
+
+        response = self.client.get(visit_count_link)
+
+        self.assertEqual(response.status_code, 200)
+        body = json_from(response)
+        self.assertIn('visit_count', body)
+        visit_count = body['visit_count']
+        self.assertEqual(visit_count, n)
+
+    def test_get_visit_link_returns_error_when_id_does_not_exist(self):
+        id = 'err'
+
+        response = self.client.get(
+            'http://localhost/{}/visit_count'.format(id)
+        )
+
+        self.assertEqual(response.status_code, 404)
+        body = json_from(response)
+        self.assertIn('error', body)
+        error = body['error']
+        self.assertIn(id, error)
